671487c29824cfbe028c6cd284b3e976dde782f6
Daniel G <eritreawow@gmail.com>
2016-04-29 15:32:03 +0100

    - v1.1

--- src/jee/eclipse/git/GitTestApp.java
+++ src/jee/eclipse/git/GitTestApp.java
@@ -1,7 +1,7 @@
 package jee.eclipse.git;
 
 public class GitTestApp {
-	public static String str = "version 1.0";
+	public static String str = "version 1.1";
 	
 	public static void main(String[] args) {
 		System.out.println(str);
