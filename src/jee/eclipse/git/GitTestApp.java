79879d5c2d201f27225e4826ea571ff43058052e
Daniel G <eritreawow@gmail.com>
2016-04-29 15:49:28 +0100

    branch bug#1234

--- src/jee/eclipse/git/GitTestApp.java
+++ src/jee/eclipse/git/GitTestApp.java
@@ -1,7 +1,7 @@
 package jee.eclipse.git;
 
 public class GitTestApp {
-	public static String str = "version 1.1";
+	public static String str = "version 1.1, from branch bug#1234";
 	
 	public static void main(String[] args) {
 		System.out.println(str);
